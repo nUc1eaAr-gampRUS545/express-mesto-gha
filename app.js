38668286aef4b19c9e9d25f6561c11eb66078d1c
nUc1eaAr-gampRUS545 <n-mutovin2003@mail.ru>
2023-08-03 01:02:14 +0300

    пофиксил выод объектов ошибок

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cookies = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
+const { errorHandler } = require('./utils/errors/errorHandler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -16,13 +17,12 @@ const { authentiacateUser } = require('./middlewares/auth');
 const { validateCreateUser, validateUserLogin } = require('./middlewares/validateJoi');
 const NotFoundError = require('./utils/errors/not-found-error');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true, useUnifiedTopology: false });
-
 app.use(bodyParser.json());
 app.use(cookies());
 app.use(cors());
 app.use(express.json());
-app.use((_req, res, next) => {
+app.use(errors());
+app.use((_req, _res, next) => {
   next(new NotFoundError('Данная страница по этому маршруту не найдена'));
 });
 app.post('/signup', validateCreateUser, createUser);
@@ -31,8 +31,7 @@ app.post('/signin', validateUserLogin, login);
 app.use('/users', authentiacateUser, routesUsers);
 app.use('/cards', authentiacateUser, routesCards);
 
-app.use(errors());
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true, useUnifiedTopology: false });
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Сервер запущен');
+  errorHandler();
 });
